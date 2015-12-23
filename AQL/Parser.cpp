5e217d89c2e95271f11110e3c34045aa1c3d9819
Almee <byuy@qq.com>
2015-12-23 17:00:28 +0800

    米开朗琪川的Parser

--- AQL/Parser.cpp
+++ AQL/Parser.cpp
@@ -5,304 +5,252 @@ Parser::Parser(Lexer* l) {
 	lookahead = lexer->scan();
 }
 
-Parser::~Parser(){
+Parser::~Parser()
+{
 }
 
-Token* Parser::getLookahead() {
-	return lookahead;
+void Parser::program() {
+	while (lookahead->tag != END) {
+		aql_stmt();
+	}
 }
 
 void Parser::match(int i) {
 	if (lookahead->tag == i) {
-		// 查看match的过程
-		//if (lookahead->tag == NUM) {
-		//	cout << static_cast<Num*>(lookahead)->value << endl;
-		//}
-		//else if (lookahead->tag >= ID &&lookahead->tag <= TOKEN) {
-		//	cout << static_cast<Word*>(lookahead)->lexeme << endl;
-		//}
-		//else {
-		//	cout << (char)lookahead->tag << endl;
-		//}
+		// 通过查看match了解匹配过程
+		if (lookahead->tag == NUM) {
+			cout << static_cast<Num*>(lookahead)->value << endl;
+		}
+		else if (lookahead->tag >= ID &&lookahead->tag <= TOKEN) {
+			cout << static_cast<Word*>(lookahead)->lexeme << endl;
+		}
+		else {
+			cout << (char)lookahead->tag << endl;
+		}
 		lookahead = lexer->scan();
 	}
 	else {
 		cout << "syntax error: line " << lexer->getline() << endl;
-		exit(1);
+		system("pause");
 	}
 }
 
-// AQL statement
+
+/*****************/
+/* AQL statement */
+/*****************/
+
+// aql_stmt -> create_stmt; | output_stmt;
 void Parser::aql_stmt() {
-	View newView;
-	pair<string, string> name;
 	switch (lookahead->tag) {
 	case CREATE:
-		match(CREATE);
-		newView = create_stmt();
-		match(';');
-		viewSet.push_back(newView);
+		create_stmt(); match(';');
 		break;
-
 	case OUTPUT:
-		match(OUTPUT);
-		name = output_stmt();
-		match(';');
-		if(name.second == "") {
-			findViewByName(name.first).print(name.first);
-		} else {
-			findViewByName(name.first).print(name.second);
-		}
+		output_stmt(); match(';');
 		break;
-
 	default:
 		cout << "syntax error: line " << lexer->getline() << ": undefined AQL statement" << endl;
-		break;
+		system("pause");
 	}
 }
 
-View Parser::create_stmt() {
-	string name;
-	vector<Column> data;
-	match(VIEW); 
-	name = static_cast<Word*>(lookahead)->lexeme;
-	match(ID); 
-	match(AS); 
-	data = view_stmt();
-	return View(name, data);
+// create_stmt -> CREATE VIEW ID AS view_stmt
+void Parser::create_stmt() {
+	match(CREATE); match(VIEW); match(ID); match(AS); view_stmt();
 }
 
-vector<Column> Parser::view_stmt() {
+// view_stmt -> select_stmt | extract_stmt
+void Parser::view_stmt() {
 	switch (lookahead->tag) {
 	case SELECT:
-		match(SELECT); 
-		return select_stmt();
+		select_stmt();
+		break;
 	case EXTRACT:
-		match(EXTRACT); extract_stmt();
+		extract_stmt();
 		break;
 	default:
 		cout << "syntax error: line " << lexer->getline() << ": undefined create statement" << endl;
-		break;
+		system("pause");
 	}
 }
 
-pair<string, string> Parser::output_stmt() {
-	string name;
-	match(VIEW);
-	name = static_cast<Word*>(lookahead)->lexeme;
-	match(ID);
-	string otherName = alias();
-	return make_pair(name, otherName);
+// output_stmt -> OUTPUT VIEM ID alias
+void Parser::output_stmt() {
+	match(OUTPUT); match(VIEW); match(ID); alias();
 }
 
-string Parser::alias() {
-	string result;
+// alias ->AS ID | E
+void Parser::alias() {
 	if (lookahead->tag == AS) {
-		match(AS);
-		result =  static_cast<Word*>(lookahead)->lexeme;
-		match(ID);
+		match(AS); match(ID);
 	}
-	return result;
 }
 
-// select statement
-vector<Column> Parser::select_stmt() {
-	Span a("123", 1, 2);
-	vector<Span> span;
-	span.push_back(a);
-	Column aa("aa", span);
-	Column bb("bb", span);
-	vector<Column> col;
-	col.push_back(aa);
-	col.push_back(bb);
-	viewSet.push_back(View("gg", col));
 
+/*******************/
+/* select statement*/
+/*******************/
 
-
-
-
-
-
-	vector<Column> result;
-	map<string, string> mapping;
-	vector<SelectTemStr> allNewCol;
-	allNewCol = select_list();
-	match(FROM);
-	mapping = from_list();
-	for (int i = 0; i < allNewCol.size(); i++) {
-		Column tmpCol(findViewByName(mapping[allNewCol[i].viewName]).findColumnByName(allNewCol[i].columnName));
-		tmpCol.setName(allNewCol[i].newName);
-		result.push_back(tmpCol);
-	}
-	return result;
+// select_stmt -> SELECT select_list FROM from_list
+void Parser::select_stmt() {
+	match(SELECT); select_list(); match(FROM); from_list();
 }
 
-vector<SelectTemStr> Parser::select_list() {
-	vector<SelectTemStr> allNewCol;
-	allNewCol.push_back(select_item());
+// select_list -> select_item | select_list, select_item
+void Parser::select_list() {
+	select_item();
 	while (lookahead->tag == ',') {
 		match(',');
-		allNewCol.push_back(select_item());
+		select_item();
 	}
-	return allNewCol;
 }
 
-SelectTemStr Parser::select_item(){
-	SelectTemStr newOne;
-	newOne.viewName = static_cast<Word*>(lookahead)->lexeme;
-	match(ID);
-	match('.');
-	newOne.columnName = static_cast<Word*>(lookahead)->lexeme;
-	match(ID);
-	newOne.newName = alias();
-	return newOne;
+// select_item -> ID.ID alias
+void Parser::select_item(){
+	match(ID); match('.'); match(ID); alias();
 }
 
-map<string, string> Parser::from_list() {
-	map<string, string> result;
-	result.insert(from_item());
+// from_list -> from_item | from_list, from_item
+void Parser::from_list() {
+	from_item();
 	while (lookahead->tag == ',') {
 		match(',');
-		result.insert(from_item());
+		from_item();
 	}
-	return result;
 }
 
-pair<string, string> Parser::from_item() {
-	string realName, nickName;
-	realName = static_cast<Word*>(lookahead)->lexeme;
-	match(ID);
-	nickName = static_cast<Word*>(lookahead)->lexeme;
-	match(ID);
-	return pair<string, string>(nickName, realName);
-
+// from_item -> ID ID
+void Parser::from_item() {
+	match(ID); match(ID);
 }
 
-// extract statement - regular
+
+/*******************************/
+/* extract statement - regular */
+/*******************************/
+
+// extract_stmt -> EXTRACT extract_spec FROM from_list
 void Parser::extract_stmt() {
-	extract_spec(); match(FROM); from_list();
+	match(EXTRACT); extract_spec(); match(FROM); from_list();
 }
 
+// extract_spec -> regex_spec | pattern_spec
 void Parser::extract_spec() {
-	//直接返回
 	switch (lookahead->tag) {
 	case REGEX:
-		match(REGEX); regex_spec();
+		regex_spec();
 		break;
 	case PATTERN:
-		match(PATTERN); pattern_spec();
+		pattern_spec();
 		break;
 	default:
 		cout << "syntax error: line " << lexer->getline() << ": undefined extract statement" << endl;
-		break;
+		system("pause");
 	}
 }
 
+// regex_spec -> REGEX REG ON column name_spec
 void Parser::regex_spec() {
-	string reg, name;
-	reg = static_cast<Word*>(lookahead)->lexeme;
-	match(REG);
-	match(ON);
-	column();
-	name = name_spec()[0].second;
-
-	/*
-	**使用正则引擎获取span填入col中 从这里生成一个只有Columb的vector<Column>向上返回即可
-	**否定上一行解决方法,与pattern在extract_spec内的返回类型冲突
-	*/
+	match(REGEX); match(REG); match(ON); column(); name_spec();
 }
 
+// column -> ID.ID
 void Parser::column() {
 	match(ID); match('.'); match(ID);
 }
 
-vector<pair<int, string> > Parser::name_spec() {
-	string name;
-	vector<pair<int, string> > result;
+// name_spec -> AS ID | RETURN group_spec
+void Parser::name_spec() {
 	switch (lookahead->tag){
 	case AS:
-		match(AS);
-		name = static_cast<Word*>(lookahead)->lexeme;
-		match(ID);
-		result.push_back(pair<int, string>(0, name));
-		return result;
+		match(AS); match(ID);
 		break;
 	case RETURN:
-		match(RETURN);
-		return group_spec();
+		match(RETURN); group_spec();
 		break;
 	default:
 		cout << "syntax error: line " << lexer->getline() << ": undefined name statement" << endl;
-		break;
+		system("pause");
 	}
 }
 
-vector<pair<int, string> > Parser::group_spec() {
-	vector<pair<int, string> > result;
-	result.push_back(single_group());
+// group_spec -> single_group | group_spec AND single_group
+void Parser::group_spec() {
+	single_group();
 	while (lookahead->tag == AND) {
 		match(AND);
-		result.push_back(single_group());
+		single_group();
 	}
-	return result;
 }
 
-pair<int, string> Parser::single_group() {
-	int number;
-	string name;
-	match(GROUP);
-	number = static_cast<Num*>(lookahead)->value;
-	match(NUM);
-	match(AS);
-	name = static_cast<Word*>(lookahead)->lexeme;
-	match(ID);
-	return pair<int, string>(number, name);
+// single_group -> GROUP NUM AS ID
+void Parser::single_group() {
+	match(GROUP); match(NUM); match(AS); match(ID);
 }
 
-// extract statement - pattern
+
+/*******************************/
+/* extract statement - pattern */
+/*******************************/
+
+// pattern_spec -> PATTERN pattern_expr name_spec
 void Parser::pattern_spec() {
-	pattern_expr();	name_spec();
+	match(PATTERN); pattern_expr(); name_spec();
 }
 
+// pattern_expr -> pattern_pkg | pattern_expr pattern_pkg
 void Parser::pattern_expr() {
 	pattern_pkg();
-	while (1) {
-		switch (lookahead->tag) {
-		case '<':
-			match('<');
-			atom();
-			if (lookahead->tag == '{') {
-				int min, max;
-				match('{');
-				min = static_cast<Num*>(lookahead)->value;
-				match(NUM);
-				match(',');
-				max = static_cast<Num*>(lookahead)->value;
-				match(NUM);
-				match('}');
-			}
-			break;
-		default:
-			break;
-		}
+	while (lookahead->tag == '<' || lookahead->tag == REG || lookahead->tag == '(') {
+		pattern_pkg();
 	}
 }
 
+// pattern_pkg -> atom | atom {NUM, NUM} | pattern_group
 void Parser::pattern_pkg() {
-
-}
-
-void Parser::atom() {
-
-}
-
-void Parser::pattern_group() {
-
-}
-
-View Parser::findViewByName(string name_) {
-	for (int i = 0; i < viewSet.size(); i++) {
-		if (viewSet[i].getName() == name_)
-			return viewSet[i];
+	switch (lookahead->tag) {
+	case '<':
+		atom();
+		if (lookahead->tag == '{') {
+			match('{'); match(NUM); match(','); match(NUM); match('}');
+		}
+		break;
+	case REG:
+		atom();
+		break;
+	case '(':
+		pattern_group();
+		break;
+	default:
+		cout << "syntax error: line " << lexer->getline() << ": undefined pattern statement" << endl;
+		system("pause");
 	}
+}
+
+// atom -> <column> | <TOKEN> | REG
+void Parser::atom() {
+	switch (lookahead->tag) {
+	case '<':
+		match('<');
+		if (lookahead->tag == ID) {
+			column();
+		}
+		else {
+			match(TOKEN);
+		}
+		match('>');
+		break;
+	case REG:
+		match(REG);
+		break;
+	default:
+		cout << "syntax error: line " << lexer->getline() << ": undefined atom statement" << endl;
+		system("pause");
+	}
+}
+
+// pattern_group -> (pattern_expr)
+void Parser::pattern_group() {
+	match('('); pattern_expr(); match(')');
 }
